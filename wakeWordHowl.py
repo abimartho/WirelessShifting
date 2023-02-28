d004f288043609e0c043bc6b275bebd397ab3b6f
abimartho <63753557+abimartho@users.noreply.github.com>
2023-02-28 09:50:08 -0700

    renames

---
+++ wakeWordHowl.py
@@ -0,0 +1,4 @@
+# Howl seems to be fully open source whereas PicoVoice's pvporcupine has a limit of
+#   100 hrs of use per month. It's a little more complicated and requires some model
+#   training but I think that it would be a good alternative
+# https://github.com/castorini/howl
